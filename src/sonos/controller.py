423fb08e0d09bcad4760faf042eacc5ca8236cca
Sam Sullivan <samsullivan09@gmail.com>
2016-11-03 16:34:13 -0500

    Adjust volume on all speakers in group.

--- src/sonos/controller.py
+++ src/sonos/controller.py
@@ -8,6 +8,9 @@ class Controller:
         self._config = Config('SonosController')
         self._speaker = speaker
 
+    def _get_all_speakers_from_group(self):
+        return self._speaker.group.members if self._speaker.group else [self._speaker]
+
     def queue_item(self, item):
         """Replace queue with a new item."""
         self._speaker.clear_queue()
@@ -15,7 +18,8 @@ class Controller:
 
     def play(self, play_mode='NORMAL'):
         """Adjust the speaker and start playing."""
-        self._speaker.play_mode = play_mode
-        self._speaker.volume = self._config.get('volume')
+        for speaker in self._get_all_speakers_from_group():
+            speaker.volume = self._config.get('volume')
 
+        self._speaker.play_mode = play_mode
         self._speaker.play()
